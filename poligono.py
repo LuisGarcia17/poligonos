dcda36d681db16f3c6578b7c2165c99643588284
Luisgarcia17 <gremorydxd007@gmail.com>
2018-08-30 17:29:31 -0500

    Inicio

---
+++ poligono.py
@@ -0,0 +1,13 @@
+class poligono():
+    lado=5
+    altura=7
+    base=10
+    ladot=11
+
+class cuadrado():
+    print("El Area de el Cuadrado es",poligono.lado*poligono.lado)
+    print("El perimetro de el Cuadrado es",poligono.lado+poligono.lado+poligono.lado+poligono.lado)
+    
+class triangulo():
+    print("El Area de el triangulo es",poligono.base*poligono.altura/2)
+    print("El perimetro de el Triangulo es",poligono.ladot+poligono.ladot+poligono.ladot)
